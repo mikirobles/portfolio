bb274f740bfbbb4c67ee4cd2deabf4cde9ddb2f0
Micael Robles <micael.robles@gmail.com>
2017-06-27 23:49:02 -0300

    better about me animation

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -11,29 +11,16 @@ $(".a.right").hover(
     }
 )
 
-$("#about").hover(
-    function () {
-        $(this).css("color", "white");
-        $("body").css("background-color", "#000990");
-    },
-    function () {
-        $(this).css("color", "black");
-        $("body").css("background-color", "#FFF");
-    }
-)
-
 
 
 let showInner;
 
 $(".a.right:first-child").hover(
+    // On mouse in
     function () {
         let animLength = 1200;
         anime({
             targets: '.dottie',
-            borderRadius: [
-                { value: 0, duration: 1 }
-            ],
             height: [
                 { value: '20px', duration: 900 },
                 { value: '600px', duration: animLength }
@@ -50,32 +37,33 @@ $(".a.right:first-child").hover(
         });
 
         showInner = setTimeout(function () {
-            if ($(".a.right:first-child").hasClass("selected")){
+            if ($(".a.right:first-child").hasClass("selected")) {
                 $(".aboutinner").css("visibility", "visible");
                 $(".aboutinner").css("opacity", "1");
+                $("body").css("background-color", "#000990");
+                $("#about").css("color", "white");
             }
         }, animLength)
     },
-    function () {
 
+    // On mouse out
+    function () {
         clearTimeout(showInner);
-        $(".aboutinner").css("visibility", "hidden");       
+        $(".aboutinner").css("visibility", "hidden");
+        $("body").css("background-color", "white");
+        $("#about").css("color", "black");
 
         anime.remove(".dottie");
         anime({
             targets: '.dottie',
             width: [
-                { value: "0px", duration: 100 }
+                { value: "0px", duration: 2 }
             ],
             height: [
-                { value: "20px", duration: 100 }
-            ],
-            borderRadius: [
-                { value: 0, duration: 700 },
-                { value: 10, duration: 100 },
+                { value: "20px", duration: 2 }
             ],
             backgroundColor: '#000',
-            duration: 1000,
+            duration: 100,
             loop: false
         })
 
