b7ba0b8519ea5d716ea3e151f13b896eb30e41a7
Micael Robles <micael.robles@gmail.com>
2017-07-02 01:03:09 -0300

    refactor js code

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,71 +1,81 @@
+var aboutItem = ".a.right:first-child";
+var aboutWindow = ".about-inner";
+var portfolioItem = ".a.right:nth-child(2)";
+var portfolioWindow = ".portfolio-inner";
+var animLength = 1200;
+
 $(".a.right").hover(
-    function () {
-        $(this).addClass("selected");
-        $(".container").css("transform", "scale(0.8)");
-        $('span:not(.selected)').css("filter", "blur(3px)");
-    },
-    function () {
-        $(this).removeClass("selected");
-        $('span:not(.selected)').css("filter", "blur(0)");
-        $(".container").css("transform", "scale(1)");
+  function() {
+    $(this).addClass("selected");
+    $(".container").css("transform", "scale(0.8)");
+    $("span:not(.selected)").css("filter", "blur(3px)");
+  },
+  function() {
+    $(this).removeClass("selected");
+    $("span:not(.selected)").css("filter", "blur(0)");
+    $(".container").css("transform", "scale(1)");
+  }
+);
+
+function leftWindowAnimation(animLength, bgColor) {
+  anime({
+    targets: ".dottie",
+    height: [
+      { value: "20px", duration: 900 },
+      { value: "600px", duration: animLength }
+    ],
+    width: [{ value: "600px", duration: animLength }],
+    translateX: [{ value: "-32%", duration: 200 }],
+    backgroundColor: bgColor,
+    duration: animLength,
+    loop: false
+  });
+}
+
+function leftWindowOutAnimation() {
+  anime.remove(".dottie");
+  anime({
+    targets: ".dottie",
+    width: [{ value: "0px", duration: 2 }],
+    height: [{ value: "20px", duration: 2 }],
+    backgroundColor: "#000",
+    duration: 100,
+    loop: false
+  });
+}
+
+function showInner(window, bodyColor, animLength) {
+  setTimeout(function() {
+    if ($(aboutItem).hasClass("selected")) {
+      $(aboutWindow).css("visibility", "visible");
+      $(aboutWindow).css("opacity", "1");
+      $("body").css("background-color", bodyColor);
+      $("#about").css("color", "white");
     }
-)
+  }, animLength);
+}
 
+function hideInner(window) {
+  $(window).css("visibility", "hidden");
+  $("body").css("background-color", "white");
+  $("#about").css("color", "black");
+}
 
-
-let showInner;
-
-$(".a.right:first-child").hover(
+function leftWindow(item, window, windowColor, bodyColor) {
+  $(item).hover(
     // On mouse in
-    function () {
-        let animLength = 1200;
-        anime({
-            targets: '.dottie',
-            height: [
-                { value: '20px', duration: 900 },
-                { value: '600px', duration: animLength }
-            ],
-            width: [
-                { value: '600px', duration: animLength }
-            ],
-            translateX: [
-                { value: "-32%", duration: 200 }
-            ],
-            backgroundColor: '#000',
-            duration: animLength,
-            loop: false
-        });
-
-        showInner = setTimeout(function () {
-            if ($(".a.right:first-child").hasClass("selected")) {
-                $(".aboutinner").css("visibility", "visible");
-                $(".aboutinner").css("opacity", "1");
-                $("body").css("background-color", "#000990");
-                $("#about").css("color", "white");
-            }
-        }, animLength)
+    function() {
+      leftWindowAnimation(animLength, windowColor);
+      showInner(window, bodyColor, animLength);
     },
-
     // On mouse out
-    function () {
-        clearTimeout(showInner);
-        $(".aboutinner").css("visibility", "hidden");
-        $("body").css("background-color", "white");
-        $("#about").css("color", "black");
-
-        anime.remove(".dottie");
-        anime({
-            targets: '.dottie',
-            width: [
-                { value: "0px", duration: 2 }
-            ],
-            height: [
-                { value: "20px", duration: 2 }
-            ],
-            backgroundColor: '#000',
-            duration: 100,
-            loop: false
-        })
-
+    function() {
+      leftWindowOutAnimation();
+      clearTimeout(showInner);
+      hideInner(window);
     }
-)
+  );
+}
+
+leftWindow(aboutItem, aboutWindow, "#000", "#000990");
+
